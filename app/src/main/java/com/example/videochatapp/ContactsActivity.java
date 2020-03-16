56de0165c710e55fa6d53ae00d7753323e03801e
TKgasa <tumelo.kgasa@gmail.com>
2020-03-16 17:02:48 +0200

    Added button styles for custom button

--- app/src/main/java/com/example/videochatapp/ContactsActivity.java
+++ app/src/main/java/com/example/videochatapp/ContactsActivity.java
@@ -2,12 +2,25 @@ package com.example.videochatapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,12 +32,21 @@ public class ContactsActivity extends AppCompatActivity
     BottomNavigationView navView;
     RecyclerView mycontactsList;
     ImageView findPeopleBtn;
+    private DatabaseReference contactsRef, userRef;
+    private FirebaseAuth mAuth;
+    private String currentUserID;
+    private String userName="", profileImage="" ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUserID = mAuth.getCurrentUser().getUid();
+        contactsRef = FirebaseDatabase.getInstance().getReference().child("Contacts");
+        userRef = FirebaseDatabase.getInstance().getReference().child("Users");
 
         navView = findViewById(R.id.nav_view);
         navView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
@@ -78,5 +100,83 @@ public class ContactsActivity extends AppCompatActivity
             return false;
         }
     };
+
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Contacts> options
+                = new FirebaseRecyclerOptions.Builder<Contacts>()
+                .setQuery(contactsRef.child(currentUserID), Contacts.class)
+                .build();
+
+        FirebaseRecyclerAdapter<Contacts, ContactsViewHolder> firebaseRecyclerAdapter
+                = new FirebaseRecyclerAdapter<Contacts, ContactsViewHolder>(options)
+        {
+            @Override
+            protected void onBindViewHolder(@NonNull final ContactsViewHolder holder, int i, @NonNull Contacts contacts)
+            {
+                final String listUserId = getRef(i).getKey();
+
+                userRef.child(listUserId).addValueEventListener(new ValueEventListener()
+                {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot)
+                    {
+                        if (dataSnapshot.exists())
+                        {
+                            userName = dataSnapshot.child("name").getValue().toString();
+                            profileImage = dataSnapshot.child("image").getValue().toString();
+
+                            holder.userNameTxt.setText(userName);
+                            Picasso.get().load(profileImage).into(holder.profileImageView);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError)
+                    {
+
+
+                    }
+                });
+            }
+
+            @NonNull
+            @Override
+            public ContactsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+            {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.contact_design, parent, false);
+                ContactsViewHolder viewHolder = new ContactsViewHolder(view);
+                return viewHolder;
+            }
+        };
+
+        mycontactsList.setAdapter(firebaseRecyclerAdapter);
+        firebaseRecyclerAdapter.startListening();
+    }
+
+    public static  class ContactsViewHolder extends  RecyclerView.ViewHolder
+    {
+
+        TextView userNameTxt;
+        Button callBtn;
+        ImageView profileImageView;
+
+
+
+
+        public ContactsViewHolder(@NonNull View itemView)
+        {
+            super(itemView);
+
+            userNameTxt = itemView.findViewById(R.id.name_contact);
+            callBtn = itemView.findViewById(R.id.call_btn);
+            profileImageView = itemView.findViewById(R.id.image_contact);
+
+        }
+    }
 
 }
